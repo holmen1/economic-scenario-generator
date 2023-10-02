77cb43821456b08a663c82f4b0be5a93c447400b
holmen1 <holmen1@gmail.com>
2023-10-02 20:46:59 +0200

    Add logging

--- project/source/api.py
+++ project/source/api.py
@@ -1,18 +1,40 @@
+import numpy as np
+import logging
+from logging.handlers import RotatingFileHandler
 from fastapi import FastAPI, Response, status
-
 from typing import List
 from pydantic import BaseModel
 from .noise import NoiseGenerator
 from .esg import EconomicScenarioGenerator
-import numpy as np
+
+
+def configure_logging():
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    file_handler = RotatingFileHandler('logs/api.log', maxBytes=100000, backupCount=1)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    uvicorn_logger = logging.getLogger("uvicorn")
+    uvicorn_logger.setLevel(logging.DEBUG)
+    uvicorn_file_handler = RotatingFileHandler('logs/api.log', maxBytes=100000, backupCount=1)
+    uvicorn_file_handler.setFormatter(formatter)
+    uvicorn_logger.addHandler(uvicorn_file_handler)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    uvicorn_logger.addHandler(stream_handler)
+
+    return logger, uvicorn_logger
 
 
 class RequestModel(BaseModel):
-    N: int              # simulations
-    T: int              # years
-    s0: List[float]     # initial value
-    a: List[float]      # speed of reversion (rates)
-    mu: List[float]     # drift (stocks), long term mean level (rates)
+    N: int  # simulations
+    T: int  # years
+    s0: List[float]  # initial value
+    a: List[float]  # speed of reversion (rates)
+    mu: List[float]  # drift (stocks), long term mean level (rates)
     sigma: List[float]  # volatility
     corrmatrix: List[List[float]]
 
@@ -25,15 +47,19 @@ class ResponseModel(BaseModel):
     vasicek: List[List[List[float]]]  # mean reversion with additive noise
 
 
+logger, uvicorn_logger = configure_logging()
+
 app = FastAPI(
     title="economic-scenario-generator",
     description="A RESTful API to generate",
     version="1.5.1",
+    logger=uvicorn_logger
 )
 
 
 @app.get("/")
 async def root():
+    logger.info("Hello!")
     return {"message": "Hello from ESG!"}
 
 
@@ -48,7 +74,7 @@ async def create_scenarios(req: RequestModel, response: Response) -> ResponseMod
     dB = NG.normal_steps(corrmatrix, N * steps)
     ESG = EconomicScenarioGenerator(s0, a, mu, sigma, dB)
 
-    # (10,2,60)
+    # (N, 2, steps)
     EQ, IR = ESG.get_scenarios(N, steps, interval)
 
     eq_list = EQ.tolist()
